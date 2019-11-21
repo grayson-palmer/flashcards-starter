3b2aac8bb03784032186259cbc9d30c81f917018
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-21 14:53:16 -0700

    Fix linting errors

--- test/Round-test.js
+++ test/Round-test.js
@@ -9,12 +9,6 @@ const Round = require('../src/Round');
 describe('Round', function() {
 
   it('should be a function', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck.cards);
-    
     expect(Round).to.be.a('function');
   });
 
