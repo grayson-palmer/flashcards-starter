a7c083ea474cc30f092fd3808ee00d81af61db49
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-21 15:10:35 -0700

    Add test for round timer

--- test/Round-test.js
+++ test/Round-test.js
@@ -130,4 +130,15 @@ describe('Round', function() {
     expect(round.endRound()).to.equal(`** Round Over! ** You answered 75% of the questions correctly!`);
   })
 
+  it('should be keep track of time for the round', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const card4 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const deck = new Deck([card1, card2, card3, card4]);
+    const round = new Round(deck);
+
+    expect(round.timer).to.equal(0);
+  })
+
 });
